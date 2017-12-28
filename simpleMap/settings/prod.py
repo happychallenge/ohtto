adb6b622be409c69692f845d939c36f85ca93153
Challenge Happy <happy@Challengeui-MacBook-Pro.local>
2017-12-28 11:19:30 +0800

    static 기존 버전으로 배포

--- simpleMap/settings/prod.py
+++ simpleMap/settings/prod.py
@@ -7,27 +7,31 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-INSTALLED_APPS += ['storages', ]  # senty 에러로깅을 위한 추가
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ join(BASE_DIR, 'staticfiles'),]
+STATIC_ROOT = join(BASE_DIR, 'static')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_S3_SECURE_URLS =False
-AWS_QUERYSTRING_AUTH = False
+# INSTALLED_APPS += ['storages', ]  # senty 에러로깅을 위한 추가
 
-AWS_REGION = 'ap-northeast-2'
-AWS_STORAGE_BUCKET_NAME = 'ohtto'
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
-AWS_ACCESS_KEY_ID = 'AKIAIWDHZGC5RTSGR4PQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# AWS_S3_SECURE_URLS =False
+# AWS_QUERYSTRING_AUTH = False
 
-# Static Setting
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# AWS_REGION = 'ap-northeast-2'
+# AWS_STORAGE_BUCKET_NAME = 'ohtto'
+# AWS_QUERYSTRING_AUTH = False
+# AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+# AWS_ACCESS_KEY_ID = 'AKIAIWDHZGC5RTSGR4PQ'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-#Media Setting
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# # Static Setting
+# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# #Media Setting
+# MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 # # sentry 에러로깅 설정
 # import raven
 
