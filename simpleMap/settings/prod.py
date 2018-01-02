eca2e5ec27970a8a809876c2c0b0a14a3c4f08d7
Challenge Happy <happy@Challengeui-MacBook-Pro.local>
2018-01-02 19:49:29 +0800

    AWS S3 delete

--- simpleMap/settings/prod.py
+++ simpleMap/settings/prod.py
@@ -10,14 +10,14 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-INSTALLED_APPS += ['storages', ]  # senty 에러로깅을 위한 추가
+# INSTALLED_APPS += ['storages', ]  # senty 에러로깅을 위한 추가
 
-AWS_ACCESS_KEY_ID = 'AKIAJYUHDGYF4ME4MRLQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'ohtto'
+# AWS_ACCESS_KEY_ID = 'AKIAJYUHDGYF4ME4MRLQ'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'ohtto'
 
-STATICFILES_STORAGE = 'simpleMap.storages.StaticS3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'simpleMap.storages.MediaS3Boto3Storage'
+# STATICFILES_STORAGE = 'simpleMap.storages.StaticS3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'simpleMap.storages.MediaS3Boto3Storage'
 
 ################################
 # For HeroKu Deploy Local 
