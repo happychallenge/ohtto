6f081f1f9e4b92106664c610bd2e9f9f9eab28c9
Challenge Happy <happychallenge@outlook.com>
2018-01-25 15:43:30 +0800

    Raven Sentry modify

--- simpleMap/settings/dev.py
+++ simpleMap/settings/dev.py
@@ -32,3 +32,46 @@ MEDIA_ROOT = join(BASE_DIR,  'media')
 # STATICFILES_STORAGE = 'simpleMap.storages.StaticS3Boto3Storage'
 # DEFAULT_FILE_STORAGE = 'simpleMap.storages.MediaS3Boto3Storage'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
